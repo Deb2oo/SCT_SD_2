a0f61c30d14ae395693e79dd6f7d016f7875f97b
debjyoti <133900810+Deb2oo@users.noreply.github.com>
2024-07-16 21:22:49 +0530

    Create number guess.c

---
+++ number guess.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int secret_number, guess;
+    srand(time(NULL));  // Seed for random number generation
+    
+    // Generate random number between 1 and 100
+    secret_number = rand() % 50 + 1;
+    
+    printf("Guess the number (between 1 and 100):\n");
+    
+    // Keep looping until user guesses correctly
+    while (1) {
+        scanf("%d", &guess);  // Read user input
+        
+        if (guess < secret_number) {
+            printf("Too low! Try again.\n");
+        } else if (guess > secret_number) {
+            printf("Too high! Try again.\n");
+        } else {
+            printf("Congratulations! You guessed the number %d!\n", secret_number);
+            break;  // Exit the loop on correct guess
+        }
+    }
+    
+    return 0;
+}
